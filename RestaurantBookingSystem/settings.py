b62c964905d5f2e6341bc72f3e47f9ff764abe29
alig2039 <alig2039@yahoo.com>
2022-04-14 22:38:54 +0000

    fixed allowed hosts

--- RestaurantBookingSystem/settings.py
+++ RestaurantBookingSystem/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-kmc#94!!!#1z8^45!2)h1tk7iy&-qiuw4xll-*=)_v+@+4t8co
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["kin-khao-ug2.herokuapp.com"]
 
 
 # Application definition
