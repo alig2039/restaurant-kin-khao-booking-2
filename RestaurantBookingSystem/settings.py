43cf2fe61e42ebe16bea28c7689b86b8dffec028
alig2039 <alig2039@yahoo.com>
2022-04-15 09:41:26 +0000

    adding gitignore from CI base

--- RestaurantBookingSystem/settings.py
+++ RestaurantBookingSystem/settings.py
@@ -13,8 +13,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 
 from pathlib import Path
-
+import os
 import dj_database_url
+if os.path.isfile("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kmc#94!!!#1z8^45!2)h1tk7iy&-qiuw4xll-*=)_v+@+4t8co'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://wbjvxhwqqmcizq:4f367b877df6bc7e09dcb1037a58d6549d5e029b3369ec5aec7b4b95e25e71cd@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/damh1vq94qn70b"
+os.environ["SECRET_KEY "] = "django-insecure-kmc#94!!!#1z8^45!2)h1tk7iy&-qiuw4xll-*=)_v+@+4t8co"
