c84c43a6c271a3819ad4c82c5011eb8eb50d59fa
alig2039 <alig2039@yahoo.com>
2022-04-18 20:25:06 +0000

    prepare to deploy to heroku

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import os
 
-os.environ["DATABASE_URL"] = "postgres://wbjvxhwqqmcizq:4f367b877df6bc7e09dcb1037a58d6549d5e029b3369ec5aec7b4b95e25e71cd@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/damh1vq94qn70b"
+os.environ["DATABASE_URL"] = "postgres://liyfxvftehfcol:3975c42dfe65c36966d8e114a62e8ed697424ac7f5533a0a3e850742d7cf6907@ec2-52-18-116-67.eu-west-1.compute.amazonaws.com:5432/d7281ijlobh07s"
 os.environ["SECRET_KEY"] = "django-insecure-kmc#94!!!#1z8^45!2)h1tk7iy&-qiuw4xll-*=)_v+@+4t8co"
 os.environ["CLOUDINARY_URL"] = "cloudinary://699721878376468:34rb3R-UQcs-YCPU9F6NTbfqxYI@allan-gerald-sserwanga"
