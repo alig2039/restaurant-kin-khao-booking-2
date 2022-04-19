e7e4e9c222f38cf83f165f50ad21b5d33cda2c60
alig2039 <alig2039@yahoo.com>
2022-04-19 10:51:50 +0000

    reinstalling requirement.txt

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import os
 
 os.environ["DATABASE_URL"] = "postgres://liyfxvftehfcol:3975c42dfe65c36966d8e114a62e8ed697424ac7f5533a0a3e850742d7cf6907@ec2-52-18-116-67.eu-west-1.compute.amazonaws.com:5432/d7281ijlobh07s"
-os.environ["SECRET_KEY"] = "django-insecure-kmc#94!!!#1z8^45!2)h1tk7iy&-qiuw4xll-*=)_v+@+4t8co"
+os.environ["SECRET_KEY"] = "rt(!g#1d#u_m5y%5bg4tk^(6qct8=o^gfnbfkdwh51ouq_&&(%"
 os.environ["CLOUDINARY_URL"] = "cloudinary://699721878376468:34rb3R-UQcs-YCPU9F6NTbfqxYI@allan-gerald-sserwanga"
