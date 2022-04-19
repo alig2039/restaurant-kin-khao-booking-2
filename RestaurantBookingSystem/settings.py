16763a43fd5ee604f5c0587c2249006b32643636
alig2039 <alig2039@yahoo.com>
2022-04-19 22:17:55 +0000

    debug change

--- RestaurantBookingSystem/settings.py
+++ RestaurantBookingSystem/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
-DEBUG = False
+
 
 if development:
     ALLOWED_HOSTS = ["localhost"]
