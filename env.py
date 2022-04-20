44a91dd3fe3b246a928458abdcd43b6cb931819d
alig2039 <alig2039@yahoo.com>
2022-04-20 09:28:23 +0000

    static files edit

--- env.py
+++ env.py
@@ -3,4 +3,4 @@ import os
 os.environ["DATABASE_URL"] = "postgres://liyfxvftehfcol:3975c42dfe65c36966d8e114a62e8ed697424ac7f5533a0a3e850742d7cf6907@ec2-52-18-116-67.eu-west-1.compute.amazonaws.com:5432/d7281ijlobh07s"
 os.environ["SECRET_KEY"] = "rt(!g#1d#u_m5y%5bg4tk^(6qct8=o^gfnbfkdwh51ouq_&&(%"
 os.environ["CLOUDINARY_URL"] = "cloudinary://699721878376468:34rb3R-UQcs-YCPU9F6NTbfqxYI@allan-gerald-sserwanga"
-os.environ['HEROKU_HOSTNAME'] = "kin-khao-ug-final.herokuapp.com" 
+os.environ['HEROKU_HOSTNAME'] = "kin-khao-ug-final.herokuapp.com"
