755bb23b5f727f1aa47906be33bcda42c8391f3e
alig2039 <alig2039@yahoo.com>
2022-04-20 22:01:34 +0000

    edit depag and impornt setting

--- RestaurantBookingSystem/settings.py
+++ RestaurantBookingSystem/settings.py
@@ -15,32 +15,31 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-from django.core.management.utils import get_random_secret_key
-
 if os.path.isfile('env.py'):
     import env
 
-# development = os.environ.get('DEVELOPMENT', False)
+development = os.environ.get('DEVELOPMENT', False)
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default=get_random_secret_key())
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-
-ALLOWED_HOSTS = [
-    'HEROKU_HOSTNAME',
-    'localhost'
-]
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -89,7 +88,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'RestaurantBookingSystem.wsgi.application'
 
-print(f"DEBUG: {DEBUG}")
 
 
 # Database
@@ -104,7 +102,7 @@ if 'DEVELOPMENT' in os.environ:
      }
 else:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 
 # Password validation
