78c2fbdb7d6af322bc1cf3edf8c1919f60023330
alig2039 <alig2039@yahoo.com>
2022-04-21 07:54:09 +0000

    new database to deploy

--- RestaurantBookingSystem/settings.py
+++ RestaurantBookingSystem/settings.py
@@ -15,11 +15,8 @@ import os
 from pathlib import Path
 from django.contrib.messages import constants as messages
 import dj_database_url
-if os.path.isfile('env.py'):
+if os.path.isfile("env.py"):
     import env
-
-development = os.environ.get('DEVELOPMENT', False)
-
 
 
 # Build paths inside the project like this:BASE_DIR/'subdir'.
@@ -39,7 +36,6 @@ DEBUG = False
 ALLOWED_HOSTS = ['kin-khao-ug-final.herokuapp.com', 'localhost']
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
-
 
 # Application definition
 
@@ -96,21 +92,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'RestaurantBookingSystem.wsgi.application'
 
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-if 'DEVELOPMENT' in os.environ:
-    DATABASES = {
-       'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-       }
-     }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+
+# DATABASES = {
+#       'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#       }
+#    }
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
