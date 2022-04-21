b1727b49791d58032bf535824eddc91f1710e7ec
alig2039 <alig2039@yahoo.com>
2022-04-21 21:11:06 +0000

    local host

--- env.py
+++ env.py
@@ -2,4 +2,3 @@ import os
 os.environ["DATABASE_URL"] = "postgres://xmvcrzpcjelwsg:3c089abbd00541600841a5dc8840b41ad08c4411a794b8ae27027a56aaa9802b@ec2-54-75-184-144.eu-west-1.compute.amazonaws.com:5432/dd38fe2hfiepeb"
 os.environ["SECRET_KEY"] = "j9i-x_dvota2l7id*9-5+kf3vy5pt6r(ehfai379ry#oop6rk("
 os.environ["CLOUDINARY_URL"] = "cloudinary://699721878376468:34rb3R-UQcs-YCPU9F6NTbfqxYI@allan-gerald-sserwanga"
-os.environ['HEROKU_HOSTNAME'] = "kin-khao-ug-final.herokuapp.com"
